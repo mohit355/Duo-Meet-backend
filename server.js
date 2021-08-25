9e6bfd5c094668efa4e4a9aa7a0a311387735500
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-08-25 15:23:49 +0530

    added base files

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const path = require("path");
+
+const port = process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+
+// imports routes
+const routes = require("./app/routes");
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Local DB connected");
+  })
+  .catch((err) => {
+    console.log("error in connecting with local DB ", err);
+  });
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+// routes middleware
+app.use("/api", routes);
+
+const io = (module.exports.io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+}));
+
+const socketManager = require("./app/socketManager");
+io.on("connection", socketManager);
+
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
